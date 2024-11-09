9772e8394e42bd6724f86272b010e5b59814c74c
Evan Glasscock <121138977+evanglass@users.noreply.github.com>
2024-11-09 14:07:29 -0800

    Timer script

---
+++ CrimsonShooter/Assets/Scripts/Utilities/Timer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private float onTime;
+
+    [SerializeField]
+    private float offTime;
+
+    [SerializeField]
+    private bool startOn;
+
+    public UnityEvent<bool> OnTimerFire;
+    public UnityEvent OnTimerFireOn;
+    public UnityEvent OnTimerFireOff;
+
+    private bool on;
+
+    private float lastFireTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        on = startOn;
+        lastFireTime = Time.realtimeSinceStartup;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (on)
+        {
+            if (Time.realtimeSinceStartup - lastFireTime > offTime)
+            {
+                on = false;
+                OnTimerFire.Invoke(on);
+                OnTimerFireOff.Invoke();
+            }
+        }
+        else
+        {
+            if (Time.realtimeSinceStartup - lastFireTime > onTime)
+            {
+                on = true;
+                OnTimerFire.Invoke(on);
+                OnTimerFireOn.Invoke();
+            }
+        }
+    }
+}
