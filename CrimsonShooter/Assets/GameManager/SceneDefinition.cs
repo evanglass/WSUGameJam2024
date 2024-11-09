32d1a2e0c36ff123a73640ab2b24f7aaac7b6fee
Evan Glasscock <121138977+evanglass@users.noreply.github.com>
2024-11-08 23:49:50 -0800

    Add scene manager

---
+++ CrimsonShooter/Assets/GameManager/SceneDefinition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneDefinition : MonoBehaviour
+{
+    [SerializeField]
+    private SceneAsset scene;
+
+    [SerializeField]
+    private SceneDefinition nextScene;
+
+    public int SceneBuildIndex
+    { 
+        get {
+            string scenePath = "Assets/Scenes/" + scene.name + ".unity";
+            return SceneUtility.GetBuildIndexByScenePath(scenePath);
+        }  
+    }
+
+    public SceneDefinition NextScene { get => nextScene; }
+
+    public void StartNextScene()
+    {
+        SceneManager.Instance.ChangeScene(this.NextScene);
+    }
+}
