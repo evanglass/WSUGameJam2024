5bc3412431dd757ee6b30fb89de69e61c6453337
Evan Glasscock <121138977+evanglass@users.noreply.github.com>
2024-11-10 18:29:46 -0800

    Nerds on level 1

--- CrimsonShooter/Assets/Scripts/Enemy/Nerd.cs
+++ CrimsonShooter/Assets/Scripts/Enemy/Nerd.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Nerd : MonoBehaviour
+public class Nerd : MonoBehaviour, ITakesShots
 {
     [SerializeField] private Rigidbody chairRB;
 
     public void ActivateChair() {
         chairRB.isKinematic = false;
+    }
+
+    public bool TakeShot(float damage)
+    {
+        GetComponent<RagdollController>().SetRagdollMode(true);
+        ActivateChair();
+        chairRB.transform.parent = transform.root;
+        return true;
     }
 }
